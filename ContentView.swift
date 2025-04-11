6791d4505230d9b8b2976e917ef17822e9ac9695
Peter Swanson <pswanson8432@stu.d214.org>
2025-04-11 11:29:44 -0700

    Make a @State variable called: number1

--- ContentView.swift
+++ ContentView.swift
@@ -2,15 +2,11 @@ import SwiftUI
 
 struct ContentView: View {
     @State var number1 = 0
-    @State var number2 = 0
-    @State var answer = 0
+  
     var body: some View {
         
         
         
         
-        Button("Calculate") {
-            answer = number1 + number2
-        }
     }
 }
