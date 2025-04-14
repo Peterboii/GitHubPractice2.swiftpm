3383df62744b99f964c1c00e898b80eacb11edfc
veerlol123 <vpatel8509@stu.d214.org>
2025-04-14 15:18:01 -0500

    Added textfield

--- ContentView.swift
+++ ContentView.swift
@@ -4,7 +4,7 @@ struct ContentView: View {
     @State var number1 = 0
     @State var number2 = 0
     var body: some View {
-        
+        TextField("Enter A Number", value: $number1, format: .number)
         
         
         
