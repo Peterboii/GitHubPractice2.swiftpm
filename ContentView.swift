f524f606322c042c2d1999d74444f573a1f3a5f7
Peter Swanson <pswanson8432@stu.d214.org>
2025-04-14 13:17:33 -0700

    Created TextField conntected variable Number2.

--- ContentView.swift
+++ ContentView.swift
@@ -4,7 +4,7 @@ struct ContentView: View {
     @State var number1 = 0
     @State var number2 = 0
     var body: some View {
-        
+        TextField("Enter a Number", value: $number2, format: .number)
         
         
         
